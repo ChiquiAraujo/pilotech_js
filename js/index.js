4998b3351e916fc9321abcb2a90c5c629f33bafb
ChiquiAraujo <ck2inf@gmail.com>
2022-10-27 23:34:46 +0200

    add pagina de tienda2

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@
 let marcs = document.querySelector("#marcas");
 marcs.innerHTML = `
 <div>
-  <img src="./img/branch/apple.svg" alt="">
+  <img src="./img/branch/apple.svg" href="./pages/tienda.html" alt="">
 </div>
 <div>
-  <img src="./img/branch/samsung.svg" alt="">
+  <img src="./img/branch/samsung.svg" href="./pages/tienda.html" alt="">
 </div>
 <div>
-  <img src="./img/branch/xiaomi.svg" alt="">
+  <img src="./img/branch/xiaomi.svg" href="./pages/tienda.html" alt="">
 </div>
 `;
